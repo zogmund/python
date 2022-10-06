44f59e0c8bf82d60f549432836246c01162f6ae7
Ambrus Zoltán <zoltan.ambrus@rendes.hu>
2022-10-06 05:39:38 +0200

    első dolgozat

---
+++ dolgozat_1.py
@@ -0,0 +1,53 @@
+# Első dolgozat. A feladatok sorszáma mellett zárójelben szerepel a pontértékük.
+# Értékelés:
+# 14-15 - Príma!
+# 12-13 - Egész jó, már csak egy kevéske hiányzik!
+# 10-11 - Már van egy alap tudásod ebből a tananyagból, de még nem elég stabil.
+# 7-9   - Látszik, hogy tanultál, de még kell gyakorolj, hogy továbblépj!
+# 0-6   - Ezt még bizony meg kell tanulni :-)
+
+
+# Javítsd a hibákat
+
+#1. (1) Írja ki, hogy: "Szép napot"
+print(Szép napot)
+
+#2. (2) Írja ki, hogy: "Sik sikert a dolgozathoz!"
+if True:
+print("Sok sikert a dolgozathoz!")
+
+#3. (2) Írja ki, hogy:
+#   2
+#   3
+if True:
+  print(2)
+   print(3)
+
+#4. (1) Írja ki a változó értékét ("kettő")
+2var = "kettő"
+print(2var)
+
+#5. (1) Írja ki, hogy: "Kettő, Három"
+x, y = "Kettő", "Három"
+print(x . ", " . y)
+
+#6. (2) Az alábbi kódrészletnek azt kellene kiírnia, hogy:
+#   1
+#   2
+#   3
+# javítsd a hibát
+i = 1
+while i < 4:
+	i += 1
+	print(i)
+
+# Old meg a feladatokat
+
+#1. (1) Irasd ki a következő három változó típusát
+f1 = "5"
+f2 = 5
+f3 = [5]
+
+#2 (3) Ciklus segítségével irasd ki a 0 és 10 közötti páratlan számokat
+
+#3 (2) Hozz létre egy Set típusú változót és irasd ki a típusát
