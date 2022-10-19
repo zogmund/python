bc9978a01f52c5d8a4113a7cb627e9649a035d56
Ambrus Zoltán <zoltan.ambrus@rendes.hu>
2022-10-20 00:08:55 +0200

    1 dolgozat v2

---
+++ dolgozat_1_2.py
@@ -0,0 +1,58 @@
+# Első dolgozat. A feladatok sorszáma mellett zárójelben szerepel a pontértékük.
+# Értékelés:
+# 14-15 - Príma!
+# 12-13 - Egész jó, már csak egy kevéske hiányzik!
+# 10-11 - Már van egy alap tudásod ebből a tananyagból, de még nem elég stabil.
+# 7-9   - Látszik, hogy tanultál, de még kell gyakorolj, hogy továbblépj!
+# 0-6   - Ezt még bizony meg kell tanulni :-)
+
+
+# Javítsd a hibákat
+
+#1. (1) Írja ki, hogy: "Jó reggelt"
+string = Jo reggelt
+print(string)
+
+#2. (1) Írja ki, hogy: "Remélem finom volt a reggeli!"
+if 1 == 1:
+print("Remélem finom volt a reggeli!")
+
+#3. (2) Írja ki, hogy:
+#   "Zabpehely"
+#   "Banánturmix"
+if 3 < 2:
+ print("Zabpehely")
+   print("Banánturmix")
+else
+	print("Sóska")
+
+#4. (1) Írja ki a változó értékét ("három")
+-3vAriable = "három"
+print(-3vAriable)
+
+#5. (2) Írja ki, hogy: "Kettő, Három"
+x, y = "Kettő", "Három"
+print(x, y)
+
+#6. (2) Az alábbi kódrészletnek azt kellene kiírnia, hogy: 4
+z, v = 2, "2"
+print(z + v)
+
+# Old meg a feladatokat
+
+#1. (1) Irasd ki a következő három változó típusát
+f1 = "5"
+f2 = 5
+f3 = [5]
+
+#2 (3) Ciklus segítségével irasd ki a 0 és 10 közötti kettővel és hárommal osztható számokat
+
+#3. (2) Az alábbi kódrészletnek azt kellene kiírnia, hogy:
+#   3
+#   2
+#   1
+# javítsd a hibát
+i = 1
+while i < 4:
+	i += 1
+	print(i)
