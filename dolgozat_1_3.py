03ef2827324afe77ad2141e11ec284f94f0461c6
Ambrus Zoltán <zoltan.ambrus@rendes.hu>
2022-11-17 05:27:51 +0100

    elso dolgozat harmadik verzio

---
+++ dolgozat_1_3.py
@@ -0,0 +1,59 @@
+# Első dolgozat v3. A feladatok sorszáma mellett zárójelben szerepel a pontértékük.
+# Értékelés:
+# 14-15 - Príma!
+# 12-13 - Egész jó, már csak egy kevéske hiányzik!
+# 10-11 - Már van egy alap tudásod ebből a tananyagból, de még nem elég stabil.
+# 7-9   - Látszik, hogy tanultál, de még kell gyakorolj, hogy továbblépj!
+# 0-6   - Ezt még bizony meg kell tanulni :-)
+
+
+# Javítsd a hibákat
+
+#1. (1) Írja ki, hogy: "Tizenkettő"
+12 = Tizenkettő
+print(12)
+
+#2. (1) Írja ki, hogy: "Mennyi az annyi?"
+if True:
+print("Mennyi az annyi?")
+
+#3. (2) Írja ki, hogy:
+#   "Sok lesz az"
+#   "Vagy kevés"
+if:
+ print("Sok lesz az")
+   print("Vagy kevés")
+else
+	print("Semennyi")
+
+#4. (1) Írja ki a változó értékét ("három")
+3var = "három"
+print(3var)
+
+#5. (2) Írja ki, hogy: "Ez sem sok"
+x, y, z = "Ez", "sem", "sok"
+print(x, y, z)
+
+#6. (2) Az alábbi kódrészletnek azt kellene kiírnia, hogy: 13
+a, b = 7, "6"
+print(a + b)
+
+# Old meg a feladatokat
+
+#1. (1) Irasd ki a következő három változó típusát
+f1 = "5"
+f2 = {5}
+f3 = [5]
+
+#2 (3) Ciklus segítségével irasd ki a 0 és 100 közötti héttel és kilenccel osztható számokat
+
+#3. (2) Az alábbi kódrészletnek azt kellene kiírnia, hogy:
+#   3
+#   2
+#   1
+#   0
+# javítsd a hibát
+i = "1"
+while i < 4:
+	i += 2
+	print(i)
