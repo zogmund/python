97025d09a206567e45e2d5f333dac2aeeb558c30
Ambrus Zoltán <zoltan.ambrus@rendes.hu>
2023-05-04 06:25:53 +0200

    dolgozat 2 v2

---
+++ dolgozat_2_1.py
@@ -0,0 +1,43 @@
+# Második dolgozat v2. A feladatok sorszáma mellett zárójelben szerepel a pontértékük.
+# Értékelés:
+# 14-15 - Príma!
+# 12-13 - Egész jó, már csak egy kevéske hiányzik!
+# 10-11 - Már van egy alap tudásod ebből a tananyagból, de még nem elég stabil.
+# 7-9   - Látszik, hogy tanultál, de még kell gyakorolj, hogy továbblépj!
+# 0-6   - Ezt még bizony meg kell tanulni :-)
+
+#1 (1) Írasd ki a lista hosszát kétféle módszerrel
+list1 = [1, 2, 3, 4, 5]
+
+# várt kimenet
+# 5
+# 5
+
+#2 (2) Adott az alábbi lista, írj egy programot, ami minden elemét megszorozza 2-vel ha páros az elem és 3-al, ha páratlan és kiírja sorban a számokat
+
+numbers2 = [1, 2, 3, 4, 5, 6, 7]
+
+# várt kimenet
+# [3, 4, 9, 8, 15, 12, 21]
+
+#3 (3) Írj egy programot, ami az alábbi listába közvetlen a 6000 után beírja, hogy 7000
+
+list3 = [10, 20, [300, 400, [[5000], 6000], 500], 30, 40]
+
+# várt kimenet:
+# [10, 20, [300, 400, [[5000], 6000, 7000], 500], 30, 40]
+
+#4 (4) Keresd meg az alábbi lista maximum és minimum elemét és irasd ki azok indexét
+
+numbers4 = [10, 32, 3, 0, 9, 60, 7]
+
+# várt kimenet
+# minimum: 3
+# maximum: 5
+
+#5 (5) Írj egy programot, ami kiszedi a listából az üres string elemeket
+
+list5 = ["Ponti", "", "Gergő", "Bálint", "", "Karolina"]
+
+# várt kimenet
+# ["Ponti", "Gergő", "Bálint", "Karolina"]
